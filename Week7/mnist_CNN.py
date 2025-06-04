5aafc3c8b7788385cb80253a11183dfe60ae5bef
Kelly Guo <guojiaxuan2001@gmail.com>
2025-06-04 13:29:04 +0900

    Week 7 complete

---
+++ Week7/mnist_CNN.py
@@ -0,0 +1,34 @@
+import tensorflow as tf 
+from tensorflow import keras
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.activations import swish
+
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)),
+    tf.keras.layers.Conv2D(32, (3, 3)),
+    BatchNormalization(),
+    tf.keras.layers.Activation(swish),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    
+    tf.keras.layers.Conv2D(64, (3, 3)),
+    BatchNormalization(),
+    tf.keras.layers.Activation(swish),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation=swish),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
